3fe59f3207c72502b796c21dd327fef0887fafae
icyrain11 <734931302@qq.com>
2023-09-17 19:24:40 +0800

    feat init

---
+++ project_template/src/test/java/com/icyrain11/project_template/ProjectTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icyrain11.project_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
